771626f7830e79d3c552f61eb15bdccf19cad934
Lennart <github2@kudling.de>
2022-07-25 20:20:54 +0200

    port to guiver 0.1.0

--- examples/island.rs
+++ examples/island.rs
@@ -1,8 +1,11 @@
-use abermals::{pattern, pattern2, wave, Canvas, Character, block, dot, heart, hash};
+use abermals::box_drawing::{
+    double_bl, double_br, double_horizontal, double_tl, double_tr, double_vertical,
+};
+use abermals::{block, dot, hash, heart, pattern, pattern2, wave, Canvas, Character};
+use druid_shell::kurbo::Size;
 use druid_shell::piet::{Color, Piet};
 use druid_shell::Region;
-use guiver::{run, Application, UserEvent, WidgetEvent, WidgetId, WidgetManager};
-use abermals::box_drawing::{double_bl, double_br, double_horizontal, double_tl, double_tr, double_vertical};
+use guiver::{run, Application, SystemEvent};
 
 struct App {
     canvas: Canvas,
@@ -10,45 +13,24 @@ struct App {
 
 impl App {
     fn new() -> Self {
-        App {
-            canvas: Canvas::new(80, 25, 2.0, Color::rgb8(0, 0, 0)),
-        }
-    }
-}
+        let mut canvas = Canvas::new(80, 25, 2.0, Color::rgb8(0, 0, 0));
 
-impl Application for App {
-    fn handle_user_event(&mut self, _user_event: &UserEvent) {}
-
-    fn handle_widget_event(
-        &mut self,
-        _widget_manager: &mut WidgetManager,
-        _widget_id: WidgetId,
-        _widget_event: &WidgetEvent,
-    ) {
-    }
-
-    fn paint(&mut self, piet: &mut Piet, region: &Region) {
-        // Paint the canvas.
-        self.canvas.paint(piet, region);
-    }
-
-    fn setup(&mut self, _widget_manager: &mut WidgetManager) {
-        let id_block = self.canvas.add_symbol(block());
-        let id_dot = self.canvas.add_symbol(dot());
-        let id_double_bl = self.canvas.add_symbol(double_bl());
-        let id_double_br = self.canvas.add_symbol(double_br());
-        let id_double_horizontal = self.canvas.add_symbol(double_horizontal());
-        let id_double_tl = self.canvas.add_symbol(double_tl());
-        let id_double_tr = self.canvas.add_symbol(double_tr());
-        let id_double_vertical = self.canvas.add_symbol(double_vertical());
-        let id_hash = self.canvas.add_symbol(hash());
-        let id_heart = self.canvas.add_symbol(heart());
-        let id_pattern = self.canvas.add_symbol(pattern());
-        let id_pattern2 = self.canvas.add_symbol(pattern2());
-        let id_wave = self.canvas.add_symbol(wave());
+        let id_block = canvas.add_symbol(block());
+        let id_dot = canvas.add_symbol(dot());
+        let id_double_bl = canvas.add_symbol(double_bl());
+        let id_double_br = canvas.add_symbol(double_br());
+        let id_double_horizontal = canvas.add_symbol(double_horizontal());
+        let id_double_tl = canvas.add_symbol(double_tl());
+        let id_double_tr = canvas.add_symbol(double_tr());
+        let id_double_vertical = canvas.add_symbol(double_vertical());
+        let id_hash = canvas.add_symbol(hash());
+        let id_heart = canvas.add_symbol(heart());
+        let id_pattern = canvas.add_symbol(pattern());
+        let id_pattern2 = canvas.add_symbol(pattern2());
+        let id_wave = canvas.add_symbol(wave());
 
         // Water.
-        self.canvas.put_rectangle(
+        canvas.put_rectangle(
             0,
             30,
             0,
@@ -61,7 +43,7 @@ impl Application for App {
         );
 
         // Grass.
-        self.canvas.put_rectangle(
+        canvas.put_rectangle(
             5,
             13,
             4,
@@ -74,7 +56,7 @@ impl Application for App {
         );
 
         // Earth
-        self.canvas.put_horizontal_line(
+        canvas.put_horizontal_line(
             6,
             14,
             11,
@@ -86,7 +68,7 @@ impl Application for App {
         );
 
         // Building.
-        self.canvas.put(
+        canvas.put(
             12,
             8,
             Character::new(
@@ -95,125 +77,130 @@ impl Application for App {
                 Some(Color::rgb8(50, 50, 50)),
             ),
         );
-        self.canvas.put(
+        canvas.put(
             12,
             7,
-            Character::new(
-                id_block,
-                Color::rgb8(150, 150, 150),
-                None,
-            ),
+            Character::new(id_block, Color::rgb8(150, 150, 150), None),
         );
 
         // Dot.
-        self.canvas.put(
+        canvas.put(
             13,
             4,
-            Character::new(
-                id_dot,
-                Color::rgb8(255, 0, 255),
-                Some(Color::rgb8(0,0,0)),
-            ),
+            Character::new(id_dot, Color::rgb8(255, 0, 255), Some(Color::rgb8(0, 0, 0))),
         );
 
         // Heart.
-        self.canvas.put(
+        canvas.put(
             8,
             8,
             Character::new(
                 id_heart,
                 Color::rgb8(255, 0, 255),
-                Some(Color::rgb8(255,255,255)),
+                Some(Color::rgb8(255, 255, 255)),
             ),
         );
 
         // Hash.
-        self.canvas.put(
+        canvas.put(
             7,
             5,
             Character::new(
                 id_hash,
                 Color::rgb8(255, 255, 0),
-                Some(Color::rgb8(255,50,0)),
+                Some(Color::rgb8(255, 50, 0)),
             ),
         );
 
         // Box drawing
-        self.canvas.put(
+        canvas.put(
             15,
             12,
             Character::new(
                 id_double_bl,
                 Color::rgb8(255, 255, 255),
-                Some(Color::rgb8(50,50,50)),
+                Some(Color::rgb8(50, 50, 50)),
             ),
         );
-        self.canvas.put(
+        canvas.put(
             21,
             12,
             Character::new(
                 id_double_br,
                 Color::rgb8(255, 255, 255),
-                Some(Color::rgb8(50,50,50)),
+                Some(Color::rgb8(50, 50, 50)),
             ),
         );
-        self.canvas.put(
+        canvas.put(
             15,
             10,
             Character::new(
                 id_double_tl,
                 Color::rgb8(255, 255, 255),
-                Some(Color::rgb8(50,50,50)),
+                Some(Color::rgb8(50, 50, 50)),
             ),
         );
-        self.canvas.put(
+        canvas.put(
             21,
             10,
             Character::new(
                 id_double_tr,
                 Color::rgb8(255, 255, 255),
-                Some(Color::rgb8(50,50,50)),
+                Some(Color::rgb8(50, 50, 50)),
             ),
         );
-        self.canvas.put(
+        canvas.put(
             15,
             11,
             Character::new(
                 id_double_vertical,
                 Color::rgb8(255, 255, 255),
-                Some(Color::rgb8(50,50,50)),
+                Some(Color::rgb8(50, 50, 50)),
             ),
         );
-        self.canvas.put(
+        canvas.put(
             21,
             11,
             Character::new(
                 id_double_vertical,
                 Color::rgb8(255, 255, 255),
-                Some(Color::rgb8(50,50,50)),
+                Some(Color::rgb8(50, 50, 50)),
             ),
         );
-        self.canvas.put_horizontal_line(
+        canvas.put_horizontal_line(
             16,
             20,
             10,
             Character::new(
                 id_double_horizontal,
                 Color::rgb8(255, 255, 255),
-                Some(Color::rgb8(50,50,50)),
+                Some(Color::rgb8(50, 50, 50)),
             ),
         );
-        self.canvas.put_horizontal_line(
+        canvas.put_horizontal_line(
             16,
             20,
             12,
             Character::new(
                 id_double_horizontal,
                 Color::rgb8(255, 255, 255),
-                Some(Color::rgb8(50,50,50)),
+                Some(Color::rgb8(50, 50, 50)),
             ),
         );
+
+        App { canvas }
     }
+}
+
+impl Application for App {
+    fn paint(&mut self, piet: &mut Piet, region: &Region) {
+        // Paint the canvas.
+        self.canvas.paint(piet, region);
+    }
+
+    fn handle_system_event(&mut self, _system_event: &SystemEvent) {}
+
+    fn resize(&mut self, _size: Size) {}
 }
 
 pub fn main() {
